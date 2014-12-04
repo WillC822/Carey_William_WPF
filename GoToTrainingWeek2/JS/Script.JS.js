4b6bb703824605cd2eaede98b3fead085e85f07a
DOC <WDCarey@fullsail.edu>
2014-12-03 23:54:54 -0500

    Training
    Age Calculator Week 2 Go To Training

--- GoToTrainingWeek2/JS/Script.JS.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by DoC822 on 12/3/14.
- */
-
-
-alert("Testing if the js file is connected.");
---
+++ GoToTrainingWeek2/JS/Script.js
@@ -0,0 +1,20 @@
+/*
+William Carey
+SDI Section # 3
+Go To Training Week #2
+Expressions Industry
+12-3-2014
+*/
+
+
+//alert("Testing if the js file is connected.");
+
+// Create a age calculator
+
+//Ask the user their name
+var name = prompt("Please type in your name");
+
+//Use the variable and say hello to the user
+alert ("Welcome "+name+"! Let's figure out how old you are");
+
+//Use
