f46dc29304b85ee1b33a5bfce96daabae0bd4d85
DOC <WDCarey@fullsail.edu>
2014-12-06 17:11:25 -0500

    Added calculation for area of a rectangle
    A=WL entered in calculations for rectangle

--- Expressions_worksheet/JS/script.js
+++ Expressions_worksheet/JS/script.js
@@ -1,3 +1,19 @@
-/**
- * Created by DoC822 on 12/4/14.
+/* William Carey
+12/6/14
+Expressions Worksheet
  */
+
+
+// A=wl calculation to find the area of a rectangle
+
+// set the width as a variable
+var width = 8;
+
+// set the length as a variable
+var length = 7;
+
+// set the variable to calculate the width times length
+var area = width * length;
+
+// set the console to provide answer
+console.log(area);
