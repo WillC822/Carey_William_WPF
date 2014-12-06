8c512c588b9cd207605b6b887384731399c7ef7b
DOC <WDCarey@fullsail.edu>
2014-12-06 16:58:03 -0500

    Created Expressions Worksheet
    Created expressionsWorksheet and JS folder

--- Expressions_Personal/JS/script.js
+++ Expressions_Personal/JS/script.js
@@ -1,3 +1,15 @@
 /**
  * Created by DoC822 on 12/4/14.
  */
+/* William Carey
+SDI Section #3
+Expressions Assignment
+Personal
+12/4/14
+ */
+
+Create a calculator
+
+Var miles
+var speed
+Var Distance
