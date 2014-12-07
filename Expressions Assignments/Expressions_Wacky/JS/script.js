99665592f81b41e6c63372cdf113315ac13b0321
DOC <WDCarey@fullsail.edu>
2014-12-07 00:01:17 -0500

    find the amount of product in a jar

--- Expressions Assignments/Expressions_Wacky/JS/script.js
+++ Expressions Assignments/Expressions_Wacky/JS/script.js
@@ -5,3 +5,19 @@
  Scalable Data Infrastructures
  12/4/14
  */
+
+//alert("testing connection")
+
+//Expressions Wacky
+
+//Calculate the amount of product in a given jar?
+
+//Ask the user the dimensions of the container
+var jarLength = prompt("What is the length of the jar?");
+var jarHeight = prompt("What is the height of the jar?");
+var jarWidth = prompt("What is the width of the jar?");
+
+//What are the dimensions of the jars occupant?
+var SizeHeight = prompt("What is the height of the occupants?");
+var SizeLength = prompt("What is the length of the occupants?");
+
