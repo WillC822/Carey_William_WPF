1401216a6ac3d9fd622f2fc7e8070cb06cccb060
DOC <WDCarey@fullsail.edu>
2014-12-06 22:57:41 -0500

    Entered in value for average time
    24 hours

--- Expressions Assignments/Expressions_Industry/JS/script.js
+++ Expressions Assignments/Expressions_Industry/JS/script.js
@@ -12,4 +12,10 @@ Scalable Data Infrastructures
 alert("Hello")
 
 //Make a calculator to show the amount of hours or projects needed to make 100,000
-var
+
+//Average time worked on project 24 hours or 3, 8 hour days
+var averageTime = 24
+
+//Average amount charged per project
+var averageProject =[100, 150, 200, 300, 500];
+
