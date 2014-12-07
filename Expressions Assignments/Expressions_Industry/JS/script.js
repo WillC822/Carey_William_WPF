1bc39aa78b779c2da9983f132019cf35cec85d53
DOC <WDCarey@fullsail.edu>
2014-12-06 23:40:18 -0500

    Completed Industry expression

--- Expressions Assignments/Expressions_Industry/JS/script.js
+++ Expressions Assignments/Expressions_Industry/JS/script.js
@@ -9,8 +9,8 @@ Scalable Data Infrastructures
 //Expressions Industry Calculator
 
 //Introduction
-alert("Hello, how much money would you like to make \n as a web designer?")
-alert("Does $100,000 sound good to you?")
+alert("Hello, how much money would you like to make \n as a web designer?");
+alert("Does $100,000 sound good to you?");
 
 //Make a calculator to show the amount of hours or projects needed to make 100,000
 
@@ -18,7 +18,7 @@ alert("Does $100,000 sound good to you?")
 var averageTime = prompt("How much hours does it take to complete \n a standard one page website?");
 
 //Average amount charged per project
-var averageProject = prompt["How much do you charge to complete a standard one page website?";
+var averageProject = prompt("How much do you charge to complete a standard one page website?");
 
 //Amount of hours in a year
 var hoursYearly = 8765.81278;
@@ -30,8 +30,8 @@ var yearlyProjects = hoursYearly/averageTime;
 var userProjection = prompt("How many projects do you feel you would need to complete \n in a years time to reach $100,000?");
 
 //Provide user with amount of projects needed and how much more or less it was than their own projection
-var finalProjection = yearlyProjects/100000;
-var difference = finalProjection - userProjection
+var finalProjection = 100000/yearlyProjects;
+var difference = finalProjection - userProjection;
 
 alert("Based on the information you provided you would need to complete "+finalProjection+ "\n in order to make $100,000 over the course of a year.");
 alert("The difference based on your estimate was " +difference+ ".");
