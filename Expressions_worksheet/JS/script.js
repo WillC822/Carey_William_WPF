e7c7ff2197b20999f15ac60a7cab774005ef51cd
DOC <WDCarey@fullsail.edu>
2014-12-06 19:04:23 -0500

    Added Calculation for Dog Years

--- Expressions_worksheet/JS/script.js
+++ Expressions_worksheet/JS/script.js
@@ -15,5 +15,30 @@ var length = 7;
 // set the variable to calculate the width times length
 var area = width * length;
 
-// set the console to provide answer
+// set the console to show answer
 console.log(area);
+
+
+
+// Find and Print the circumference of a circle
+//Calculate the circumference of a circle
+
+var radius = 6;
+const PI = 3.14159265
+
+//multiply 2, PI and the raius & assign to circ variable
+var circ = radius *2 * Pi;
+//print out the output circumference
+console.log("The circumfernce of the circle is" + circ);
+
+
+
+//Dog years
+//Enter in the variable for dog years
+var dogYears= 7;
+
+//Enter in the variable for human years
+var humanYears = 30
+
+//Set the variable for sparkys age by dogYears times humanYears
+var sparkysAge = dogYears * humanYears
