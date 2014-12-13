bc0ffbde81391ff7aebf9b184d56d3775f53a80d
DOC <WDCarey@fullsail.edu>
2014-12-12 21:24:21 -0500

    changed wacky to kite flying

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -9,11 +9,33 @@
  */
 
 
-//go out clubbin??? //
-// did you get an invite yes/no -
-// got an invite, where we going???
-// who alls going
-// where are my kids
-// What'd the wife say
-// who's driving
-// what time will i be home
+
+//Flying a kite
+//what's the wind speed
+//is it an open area
+// are you willing to run
+// can you tell which way the wind is blowing
+
+alert("Let's try to fly a kite!");
+
+//is it a windy day
+
+var windy = true;
+var windSpeed;
+var windCond;
+
+windCond = (windSpeed < 5);
+
+var windCond = prompt("Is it windy outside?");
+
+var openArea = prompt("Are you in an open area?");
+
+//if it is a windy day then we will go outside and fly a kite
+if(windy==true){
+ //will let you know we can fly a kite because conditions are good
+ console.log("Let's go fly a kite!");
+}else{
+ console.log("Wait for the wind to pick up!");}
+or
+
+
