45cba52f41b6a80b0462a2b0b6affce501aa140c
DOC <WDCarey@fullsail.edu>
2014-12-13 00:49:21 -0500

    made if statement for overtime

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -21,17 +21,18 @@ var hoursWorked = prompt("How many hours did you work for your last 2 week pay p
 
 //Did they work ovetime also?
 var overTime = prompt("Did you work any overtime?");
-
-//Find out time over
-var timeOver = prompt("How much overtime did you work?");
-
+if(overTime = "yes");{
+ //Find out time over
+ var timeOver = prompt("How much overtime did you work?");
+}
 //Find out Hourly Pay
 var hourlyPay = prompt("How much do you get paid hourly?");
 
 //Calculate base pay
 var basePay = hoursWorked*hourlyPay;
+
 //Calculate base pay+time over
-var overtimePay = timeOver*1.5
+var overtimePay = timeOver*1.5;
 
 /*if(hoursWorked==80) console.log("Based on the information provided you will get a gross pay of " +basePay+ "!");{
  else
