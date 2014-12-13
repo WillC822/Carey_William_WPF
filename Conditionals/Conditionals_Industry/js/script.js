00686d7c48f7284c299ad2b03ff9070824c993fb
DOC <WDCarey@fullsail.edu>
2014-12-13 00:40:59 -0500

    completed else statement

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -17,34 +17,37 @@
 alert("Let's find out what your paycheck is going to be.");
 
 //Find out hours worked in pay period(Bi-Weekly)
-var hoursWorked = prompt("How many hours did you work for your last 2 week pay period?");
+var hoursWorked = prompt("How many hours did you work for your last 2 week pay period?\n Not including overtime.");
 
 //Did they work ovetime also?
 var overTime = prompt("Did you work any overtime?");
 
+//Find out time over
+var timeOver = prompt("How much overtime did you work?");
 
 //Find out Hourly Pay
 var hourlyPay = prompt("How much do you get paid hourly?");
 
 //Calculate base pay
 var basePay = hoursWorked*hourlyPay;
+//Calculate base pay+time over
+var overtimePay = timeOver*1.5
 
-if(hoursWorked==80) console.log("Based on the information provided you will get a gross pay of " +basePay+ "!");{
+/*if(hoursWorked==80) console.log("Based on the information provided you will get a gross pay of " +basePay+ "!");{
  else
-}prompt("How many hours of overtime did you work?");
-
-if (overtime=true) hourlyPay*1.5+basePay==overTime;
+ if (overtime=true) hourlyPay*1.5+basePay==overTime;
 alert("Based on the information provided you will get a gross pay of " +basePay+ "!");
 console.log("Based on the information provided you will get a gross pay of " +basePay+ "!");
-
-if(overtime===true){
- console.log
+*/
+if(hoursWorked<=80){
+ alert("Based on the information provided you will get a gross pay of " +basePay+ "!");
+ console.log("Based on the information provided you will get a gross pay of " +basePay+ "!");
+}else{
+ alert("Based on the information provided you will get a gross pay of " +basePay+overtimePay+ "!");
+ console.log("Based on the information provided you will get a gross pay of " +basePay+overtimePay+ "!");
+}
 }
 
-//what's my paycheck going to be
-//how many hours did i work
-//did i do anything freelance
-//did i finish all my jobs on time
 
 
 
