748015a9d02b61bd5e6c1abaafb427817b40eda9
DOC <WDCarey@fullsail.edu>
2014-12-13 12:58:15 -0500

    changed the options to show true or false

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -15,40 +15,35 @@
 //is it an open area
 // are you willing to run
 // can you tell which way the wind is blowing
-
-alert("Let's try to fly a kite!");
+//Alert user to intentions
+alert ("Let's try to fly a kite!");
 
 //is it a windy day
 
 var windy = true;
-var windSpeed = ;
+
+//are you willing to run
 var run = true;
-var windCond;
 
 //prompt to ask about the conditions outside
-var windCond = prompt("Is it windy outside?");
+var windCond = prompt("Is it windy outside?"[true || false];
 
 //conditional to check for empty answer
-if(windCond===""){
+if(windCond ===""){
 
  //Re-prompt the user
- windCond=prompt("Is it windy outside, your input is greatly appreciated.");
+ windCond = prompt("Is it windy outside, your input is greatly appreciated.");
 
 //prompt to ask if there's enough space to fly a kite
-var openArea = prompt("Are you in an open area?");
+var openArea = prompt("Are you in an open area?"(true || false);
 
 //if it is a windy day then we will go outside and fly a kite
-if(windy==true && openArea==true){
+if(windy===true && openArea===true){
  //will let you know we can fly a kite because conditions are good or if you're willing to run we can fly a kite that way
  console.log("Let's go fly a kite!");
-}else if(windCond!==true && run==true){
+}else if(windCond!==true && run===true){
  console.log("If you don't mind running maybe we can get it up to speed that way");
  }else{
  console.log("Wait for the wind to pick up!");
 }
 
-if(windCond===""){
- //the user left it blank
-
- //Re-prompt the user
- windCond=prompt("Is it windy outside, your input is greatly appreciated.");
