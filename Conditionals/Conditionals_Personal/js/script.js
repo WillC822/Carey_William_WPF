689922eeaa98ef757b64a5ccf77c79027ec530ab
DOC <WDCarey@fullsail.edu>
2014-12-12 21:26:15 -0500

    Changed personal to information about Leo

---
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Created by DoC822 on 12/11/14.
+ */
+/*
+ William Carey
+ SDI Section # 3
+ Conditionals Personal
+ 12-11-2014
+ */
+
+//My baby is in NICU in the hospital, in order for him to leave he must pass a few goals
+
+//My baby must be weigh at least 4lbs
+//He must be breathing on his own
+//He must eat on his own
+
+//Leo is my babies name, he was born Nov 16 2014 at 2lbs, 6oz
+var babyLeo =  "Leo";
+//set a variable for baby's weight
+var babyWeight = (0);
+//set variable to show any weight greater than 4lbs makes him good to go
+var i=0
+//set variable to confirm baby eating
+var babyEating= True;
+//set variable to confirm baby's breathing on his own
+var babyOxy= True;
+
