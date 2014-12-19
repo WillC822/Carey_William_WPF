42c32523d024bb32032a02093410ff01ed290c19
DOC <WDCarey@fullsail.edu>
2014-12-19 02:27:13 -0500

    set up array
    tried setting array to pick between two different and provide common as
    answer

--- FunctionsWeek4/personalFunctions/js/personalF.js
+++ FunctionsWeek4/personalFunctions/js/personalF.js
@@ -26,34 +26,27 @@ if(hungry==="YES"|| hungry==="Yes"|| hungry==="yes"){
 var taste = prompt("Are you in the mood for BBQ, Fish Tacos, Fast Food, Italian, Japanese or Tex-Mex?");
 
 //Where should we go to eat for dinner.
-var rest =["4Rivers","Jimmy Hulas","Zaxbys","Olive Garden","Kobe", "Chevys"];
+var restArray1 =["4Rivers","Jimmy Hulas","Zaxbys","Olive Garden","Kobe", "Chevys"];
 
 //What are you in the mood for
-var mood =["BBQ","Fish Tacos","Fast Food", "Italian", "Japanese","TexMex"];
-
-var restBBQ = mood[0]=rest[0]
-var restFISHTACOS = mood[1]=rest[1]
-var restFASTFOOD = mood[2]=rest[2]
-var restITALIAN = mood[3]=rest[3]
-var restJAPANESE = mood[4]=rest[4]
-var restTexMex = mood[5]=rest[5]
-
+var moodArray2 =["BBQ","Fish Tacos","Fast Food", "Italian", "Japanese","TexMex"];
+/*
 //How much are you able to spend
 var price = prompt("Are you looking for someplace cheap, moderate or expensive");
 
 var pricePoint=["cheap", "moderate", "expensive"];
+*/
+//Use a function to input all the answers and dependent on the answers given choose a corresponding restaurant
+function restChoice(a,b,c,d,e,f){
+    console.log();
 
-
-
-//Use a function to input all the answers and dependent on the answers given choose a corresponding restaraunt
-function restChoice(prcPnt,tste){
-    console.log()
-
-
+    var a = moodArray2[0]=restArray1[0];
+    var b = moodArray2[1]=restArray1[1];
+    var c = moodArray2[2]=restArray1[2];
+    var d = moodArray2[3]=restArray1[3];
+    var e = moodArray2[4]=restArray1[4];
+    var f = moodArray2[5]=restArray1[5];
+    return restChoice(taste&&"a"||"b"||"c"||"d"||"e"||"f");
     }
-}
-restChoice(pricePoint,taste);
 
-(rest===
-
-
+console.log(restChoice())
