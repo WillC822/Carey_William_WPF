5cfd8cecfb3a884091db6a67c0ad05f07a46e6c3
DOC <WDCarey@fullsail.edu>
2014-12-18 20:51:16 -0500

    brainstorming personal ideas

--- FunctionsWeek4/personalFunctions/js/personalF.js
+++ FunctionsWeek4/personalFunctions/js/personalF.js
@@ -6,6 +6,11 @@
 
 //alert("test to see if code works");
 
+//Where should we go to eat for dinner
+var rest =["Jimmy Hulas","Texas Roadhouse","Zaxbys","Olive Garden","Bahama Breeze", "Kobe", "Chevys","4Rivers"];
+
+//What are you in the mood for
+var mood =["BBQ","Island Tacos","American", "Italian", "Japanese","Mexican",];
 
 
-//We're going to write to determine who my secret santa may be.
+
